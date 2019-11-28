238398dcd5d6717315a41ddf313a14aaab9ba631
aSousukeFujii <sousuke.fujii@access-company.com>
2019-11-28 15:15:31 +0900

    [193875] add table 'amazon_coupons'

---
+++ migrations/20191128050700-create-amazon-coupons.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('amazon_coupons', {
+      uid: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      point: {
+        type: Sequelize.INTEGER
+      },
+      point_url: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('line_points');
+  }
+};
