7985158d06714de6ca240fd8619b8d52b725a82b
Fumihiro Saito <Fumihiro.Saito@access-company.com>
2019-12-05 11:42:50 +0900

    [192641] Update GET bodygram estimation (#31)
    * Add sequelize model.
    
    * Add sequelize and update notifyEstimation.
    
    * Update POST users.
    
    * Update GET and PUT API.
    
    * Update Get /bodygram/estimation.

--- src/functions/getBodygramEstimation/index.ts
+++ src/functions/getBodygramEstimation/index.ts
@@ -1,64 +1,46 @@
 import { APIGatewayEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
+import { authorize } from '../../lib/authorize'
+import { OK, UnAuthorized, BadRequest, NotFound, ServerError } from '../../lib/response'
+import { convertResponse } from '../../model/estimation_log'
+
+import db from '../../db/models'
+
 // const env = process.env
 
 const handler: APIGatewayProxyHandler = async (event: APIGatewayEvent) => {
-    if (event.httpMethod !== 'GET') {
-        throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`)
-    }
-    if (!event.queryStringParameters || !event.queryStringParameters.rid) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({message: 'Bad Request.'}),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }
-    }
-    // All log statements are written to CloudWatch
-    // console.info('received:', event)
-    // console.log(env)
+  if (event.httpMethod !== 'GET') {
+    throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`)
+  }
+  // All log statements are written to CloudWatch
+  // console.info('received:', event)
+  // console.log(env)
+  if (!event.queryStringParameters || !event.queryStringParameters.rid) {
+    return BadRequest('Query parameter invalid.')
+  }
+  const userId: string | undefined = await authorize(event)
+  if (!userId) {
+    return UnAuthorized('LINE token unauthorized.')
+  }
 
-    let response: APIGatewayProxyResult
+  let response: APIGatewayProxyResult
 
-    try {
-        // TODO: Get user_id from API header token or query.
-        const rid = event.queryStringParameters ? event.queryStringParameters.rid : null
-        console.log(rid)
-        // TODO: get estimation_log from Mysql(RDS).
-        response = {
-            statusCode: 200,
-            body: JSON.stringify({ // 仮レスポンス
-                id: 1,
-                rid,
-                uid: 'user_id',
-                status: 'requested',
-                waist_circumference: 80,
-                offal_fat: 20,
-                wc_diff: 0,
-                of_diff: 0,
-                week: 1,
-                created_at: '2019-11-13 18:34:52',
-                updated_at: '2019-11-13 18:34:52',
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }
-    } catch(e) {
-        response = {
-            statusCode: 500,
-            body: JSON.stringify(e),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }
+  try {
+    const rid = event.queryStringParameters.rid
+    const log = await db.estimationLogs.findByPk(rid)
+    if (log) {
+      response = OK(convertResponse(log))
+    } else {
+      response = NotFound('Estimation log not found.')
     }
+  } catch(e) {
+    response = ServerError(e)
+  }
 
-    // All log statements are written to CloudWatch
-    console.info(event.headers['X-Api-Key'])
-    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`)
-    return response
+  // All log statements are written to CloudWatch
+  console.info(event.headers['X-Api-Key'])
+  console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`)
+  return response
 }
 
 export {handler}
